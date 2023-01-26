e15494b23f9f1897bbb7683726c063a01c7c6ee4
Stelios Zafeiris <stelios.zafeirhs@hotmail.gr>
2023-01-26 19:58:51 +0200

    Implemented configuration

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from src.main import *
+from src.main.configurator import configurator as conf
 
+from logging.config import dictConfig
+import logging
+
+dictConfig(conf._LOGGING_CONFIG_)
+log = logging.getLogger('main.configurator')
+
+print(conf.LOG_LEVEL)
+print(conf.getAll())
+print('blah' in conf)
+print('LOG_LEVEL' in conf)
+
+log.debug('Hello world!')
+log.info('Hello world!')
+log.error('Hello world!')
+log.critical('Hello world!')
