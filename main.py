3c3e23c9f5b8a2e48d2a2d04c0254af89b26d5b3
Stelios Zafeiris <stelios.zafeirhs@hotmail.gr>
2023-02-01 23:32:31 +0200

    Update main.py according to recent changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.main.configurator import configurator as conf
-from src.main.data import *
-from src.main.algorithm import *
+from src.main.data import NsclcRadiogenomicsDataService
+from src.main.evaluation import *
+
+import os
 
 ## Logging setup
 from logging.config import dictConfig
@@ -9,16 +11,17 @@ import logging
 dictConfig(conf._LOGGING_CONFIG_)
 log = logging.getLogger()
 
-dicomDataService = DataService(DicomReader())
+dataService = NsclcRadiogenomicsDataService()
 
 # Convert folders to nifty
-# dicomDataService.convertToNifty(conf.NSCLC_IMAGES_DIR, conf.NIFTI_IMAGES_DIR)
+if not os.path.exists(conf.NSCLC_NIFTI_IMAGES_DIR):
+    dataService.convertToNifty(conf.NSCLC_IMAGES_DIR, conf.NSCLC_NIFTI_IMAGES_DIR)
 
 # Extract radiomics from images or read them
-if os.path.exists(conf.RADIOMICS_FILE):
-    radiomicFeatures = pd.read_csv(conf.RADIOMICS_FILE)
+if os.path.exists(conf.NSCLC_RADIOMICS_FILE):
+    radiomicFeatures = dataService.readRadiomics(conf.NSCLC_RADIOMICS_FILE)
 else:
-    radiomicFeatures = dicomDataService.extractRadiomics(conf.NIFTI_IMAGES_DIR, conf.RADIOMICS_FILE)
+    radiomicFeatures = dataService.extractRadiomics(conf.NSCLC_NIFTI_IMAGES_DIR, conf.NSCLC_RADIOMICS_FILE)
 
 patientIds = radiomicFeatures.pop('Patient_Id').to_list()
 radiomicFeaturesNames = radiomicFeatures.columns.to_list()
@@ -28,7 +31,11 @@ X = radiomicFeatures.to_numpy()
 y = np.asarray([0, 0, 1, 1, 0, 0])
 
 # Execute mRMR
-mrmr = mRMR()
-result = mrmr.fit_transform(X, y, featureNames=radiomicFeaturesNames)
-log.debug(result.shape)
-log.debug(mrmr.get_params())
+pipeline = Pipeline([
+    ('fs', mRMR()),
+    ('model', SVC())
+])
+
+pipeline.fit(X, y)
+log.debug(pipeline)
+
