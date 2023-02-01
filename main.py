ab758e80e0bab8f98139e029f21a156c7af04f94
Stelios Zafeiris <stelios.zafeirhs@hotmail.gr>
2023-02-01 17:21:25 +0200

    Implemented mRMR fs algorithm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.main.configurator import configurator as conf
 from src.main.data import *
+from src.main.algorithm import *
 
 ## Logging setup
 from logging.config import dictConfig
@@ -15,9 +16,18 @@ dicomDataService = DataService(DicomReader())
 
 # Extract radiomics from images or read them
 if os.path.exists(conf.RADIOMICS_FILE):
-    radiomicFeatures = pd.read_csv(conf.RADIOMICS_FILE).to_numpy()
+    radiomicFeatures = pd.read_csv(conf.RADIOMICS_FILE)
+    patientIds = radiomicFeatures.pop('Patient_Id').to_list()
+    radiomicFeaturesNames = radiomicFeatures.columns.to_list()
+    X = radiomicFeatures.to_numpy()
 else:
     radiomicFeatures = dicomDataService.extractRadiomics(conf.NIFTI_IMAGES_DIR, conf.RADIOMICS_FILE)
 
 # Declare labels
 y = np.asarray([0, 0, 1, 1, 0, 0])
+
+# Execute mRMR
+mrmr = mRMR()
+result = mrmr.fit_transform(X, y, featureNames=radiomicFeaturesNames)
+log.debug(result.shape)
+log.debug(mrmr.get_params())
--- src/main/algorithm.py
+++ src/main/algorithm.py
@@ -1,16 +1,26 @@
-from sklearn.base import BaseEstimator
+from src.main.configurator import configurator as conf
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.exceptions import NotFittedError
 
-class FeatureSelectionAlgorithm(BaseEstimator):
+from ITMO_FS.filters.multivariate import MRMR
+
+import numpy as np
+
+## Logging setup
+from logging.config import dictConfig
+import logging
+
+dictConfig(conf._LOGGING_CONFIG_)
+log = logging.getLogger()
+
+class FeatureSelectionAlgorithm(BaseEstimator, TransformerMixin):
     def __init__(self, estimator=None):
         self.__estimator = estimator
     
-    def fit(self, X, y, **kargs):
+    def fit(self, X, y=None, **kwargs):
         self.__estimator.fit(X, y)
 
-    def fit_transform(self, X, y, **kargs):
-        self.__estimator.fit_transform(X, y)
-
-    def transform(self, X, y, **kargs):
+    def transform(self, X, y=None, **kwargs):
         self.__estimator.transform(X, y)
 
     def get_params(self, deep=True):
@@ -21,23 +31,51 @@ class FeatureSelectionAlgorithm(BaseEstimator):
             setattr(self.__estimator, parameter, value)
         return self
 
-# TODO: Implement mRMR feature selection method
-class MRMR(FeatureSelectionAlgorithm):
-    def __init__(self, k, estimator=None):
+class mRMR(FeatureSelectionAlgorithm):
+    def __init__(self, k=10, estimator=None):
         super().__init__(estimator)
-        self._k = k
+        self.k = k
+        self._isFitted_ = False
 
-    def fit(self, X, y, **kargs):
-        self.__estimator.fit(X, y)
+    def fit(self, X: np.array, y: np.array, **kwargs):
+        features = [i for i in range(X.shape[1])]
+        selectedFeatures = []
+        freeFeatures = [i for i in features if i not in selectedFeatures]
 
-    def fit_transform(self, X, y, **kargs):
-        self.__estimator.fit_transform(X, y)
+        while len(selectedFeatures) != self.k:
+            mrmr = MRMR(np.asarray(selectedFeatures), np.asarray(freeFeatures), X, y)
+            mxPos = np.argmax(mrmr)
+            log.debug(f"mrmr {mrmr}, mxPos {mxPos}")
 
-    def transform(self, X, y, **kargs):
-        self.__estimator.transform(X, y)
+            selectedFeatures.append(freeFeatures[mxPos])
+            freeFeatures = [i for i in features if i not in selectedFeatures]
+
+        featureNames = None
+        self._selectedFeaturesNames = None
+        if 'featureNames' in kwargs:
+           featureNames = kwargs['featureNames']
+
+        if featureNames is not None:
+            selectedFeaturesNames = [featureNames[s] for s in selectedFeatures]
+            self._selectedFeaturesNames = selectedFeaturesNames
+        
+        self._selectedFeatures = selectedFeatures
+        self._isFitted_ = True        
+    
+        return self
+
+    def transform(self, X, y=None, **kwargs):
+        if not self._isFitted_:
+            raise NotFittedError()
+        
+        return X[:, self._selectedFeatures]
 
     def get_params(self, deep=True):
-        return {'_best_features': self._best_features, 'k': self._k}
+        return {
+            'selected_features_': self._selectedFeatures,
+            'k': self.k,
+            'selected_feature_names_': self._selectedFeaturesNames if self._selectedFeaturesNames else '',
+        }
 
     def set_params(self, **parameters):
         for parameter, value in parameters.items():
