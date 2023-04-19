d61815ed6f7c1ee7c15fc34f2ee8cd84609636f0
Stelios Zafeiris <stelios.zafeirhs@hotmail.gr>
2023-04-19 16:46:36 +0300

    Changes

--- src/main/notification.py
+++ src/main/notification.py
@@ -4,8 +4,8 @@ from .configurator import configurator as conf, log
 def send_to_telegram(message):
     apiURL = f'https://api.telegram.org/bot{conf.TELEGRAM_TOKEN}/sendMessage'
     try:
-        if conf.TELEGRAM_SEND:
-            response = requests.post(apiURL, json={'chat_id': conf.TELEGRAM_CHAT_ID, 'text': message}, )
+        # if conf.TELEGRAM_SEND:
+        response = requests.post(apiURL, json={'chat_id': conf.TELEGRAM_CHAT_ID, 'text': message}, )
     except Exception as e:
         log.error(str(e))
         log.exception(e)
