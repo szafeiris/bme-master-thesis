2f23a1a7e186f3c83138bc78c1dda1f8bb2d0bd1
Stylianos Zafeiris <stelios.zafeirhs@hotmail.gr>
2023-09-14 15:51:27 +0300

    🐛 Fix telegram notifications issue

--- src/main/notification.py
+++ src/main/notification.py
@@ -1,12 +1,13 @@
 from .configuration import configuration as conf, log
 import requests
+import json
 
 def send_to_telegram(message):
     apiURL = f'https://api.telegram.org/bot{conf.TELEGRAM_TOKEN}/sendMessage'
     try:
         if conf.TELEGRAM_SEND:
             response = requests.post(apiURL, json={'chat_id': conf.TELEGRAM_CHAT_ID, 'text': message}, )
-            log.debug("Response from telegram API: \n", response.raw)
+            log.debug(f"Response from telegram API: \n{json.dumps(response.json(), indent=2)}\n")
         else:
             log.debug("No request send to telegram API.")
             
