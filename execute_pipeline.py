27be51fd8d679bbb06562c9c946598a4d8e6cf43
Stylianos Zafeiris <stelios.zafeirhs@hotmail.gr>
2023-12-13 14:27:04 +0200

    ♻️ Updated pipeline execution

--- execute_pipeline.py
+++ execute_pipeline.py
@@ -8,22 +8,7 @@ def executePipeline(pipeline: Pipeline):
     pipeline.run()
 
 def executeFullCombinationPipelinesAsync():
-    originalPipeline: Pipeline = FullCombinationPipeline(Datasets.ORIGINAL)
-    originalNormalizedPipeline: Pipeline = FullCombinationPipeline(Datasets.ORIGINAL_NORMALIZED)
-    n4Pipeline: Pipeline = FullCombinationPipeline(Datasets.N4)
-    n4NormalizedPipeline: Pipeline = FullCombinationPipeline(Datasets.N4_NORMALIZED)
-    fatNormalizedPipeline: Pipeline = FullCombinationPipeline(Datasets.FAT_NORMALIZED)
-    muscleNormalizedPipeline: Pipeline = FullCombinationPipeline(Datasets.MUSCLE_NORMALIZED)
-    
-    processes = [
-        Process(target=executePipeline, args=(originalPipeline,)),
-        Process(target=executePipeline, args=(originalNormalizedPipeline,)),
-        Process(target=executePipeline, args=(n4Pipeline,)),
-        Process(target=executePipeline, args=(n4NormalizedPipeline,)),
-        Process(target=executePipeline, args=(fatNormalizedPipeline,)),
-        Process(target=executePipeline, args=(muscleNormalizedPipeline,)),
-    ]
-    
+    processes = [ Process(target=executePipeline, args=(FullCombinationPipeline(dataset),)) for dataset in Datasets._ALL_DATASETS ]
     for p in processes:
         p.start()
         
